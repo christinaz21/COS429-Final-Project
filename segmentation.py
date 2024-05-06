bb6b2989a8d1d933d38cd8cb194e3bef5acdc28c
Christina Zhang <123713200+christinaz21@users.noreply.github.com>
2024-05-05 22:29:49 -0400

    Add files via upload

---
+++ segmentation.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+import torch
+from torchvision.models.detection import maskrcnn_resnet50_fpn_v2
+from torchvision.transforms import functional as F
+import matplotlib.pyplot as plt
+import os
+
+def load_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+def apply_mask_rcnn(image):
+    model = maskrcnn_resnet50_fpn_v2(weights='DEFAULT')
+    model.eval()
+    transform = F.to_tensor(image).unsqueeze(0)
+    with torch.no_grad():
+        prediction = model(transform)
+    return prediction[0]
+
+def apply_grabcut(image, mask):
+    #for i in range(mask.shape[0]):
+       # for j in range(mask.shape[1]):
+            #print(mask[i, j])
+    #plt.imshow(image)
+    #plt.plot(mask)
+    #plt.show()
+    mask[mask == 1] = cv2.GC_FGD  # Foreground
+    mask[mask == 0] = cv2.GC_BGD  # Background
+    #print(mask)
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+    mask, bgdModel, fgdModel = cv2.grabCut(image, mask, None, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_MASK)
+    #plt.imshow(mask)
+    return (mask == cv2.GC_FGD) | (mask == cv2.GC_PR_FGD)
+
+def segment_image(image_path):
+    image = load_image(image_path)
+    outputs = apply_mask_rcnn(image)
+    #print(outputs)
+    masks = outputs['masks'] > 0.5
+    mask_combined = np.zeros(masks.shape[-2:], dtype=np.uint8)
+    if masks.numel() > 0:
+        mask_combined = masks[0, 0].byte().cpu().numpy()
+    #plt.imshow(mask_combined)
+    segmented = apply_grabcut(image, mask_combined)
+    #plt.imshow(segmented)
+    return image, segmented
+
+def process_images(input_folder, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    count = 0
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(input_folder, filename)
+            output_path = os.path.join(output_folder, filename)
+
+            try:
+                original_image, segmented_mask = segment_image(image_path)
+                background_image = original_image.copy()
+                background_image[segmented_mask] = 0
+                cv2.imwrite(output_path, cv2.cvtColor(background_image, cv2.COLOR_RGB2BGR))
+                #print(f"Processed and saved background image to {output_path}")
+                count += 1
+                if count % 50 == 0:
+                    print("processed ", count)
+                
+            except Exception as e:
+                print(f"Failed to process {image_path}: {e}")
+
+# change input and output folder for each scene
+input_folder = 'sky_occluded'
+output_folder = 'sky_processed'
+process_images(input_folder, output_folder)
